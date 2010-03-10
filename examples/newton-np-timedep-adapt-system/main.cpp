0d298e6f6fe432b0e4379b41a3a9daa454777e23
David Pugal <david.pugal@gmail.com>
2010-03-10 11:40:30 -0800

    Crank-Nicols timestepping for Nernst-Planck example

--- examples/newton-np-timedep-adapt-system/main.cpp
+++ examples/newton-np-timedep-adapt-system/main.cpp
@@ -66,6 +66,7 @@ const double TAU = 0.1;              // Size of the time step
 const int P_INIT = 2;       	        // Initial polynomial degree of all mesh elements.
 const int REF_INIT = 10;     	        // Number of initial refinements
 const bool MULTIMESH = true;	        // Multimesh?
+const int TIME_DISCR = 1;             // 1 for implicit Euler, 2 for Crank-Nicolson
 
 /* Nonadaptive solution parameters */
 const double NEWTON_TOL = 1e-2;         // Stopping criterion for nonadaptive solution
@@ -433,15 +434,19 @@ int main (int argc, char* argv[]) {
   // a11(u1, v1) + a12(u2, v1) + a1n(un, v1) = l1(v1)
   // a21(u1, v2) + a22(u2, v2) + a2n(un, v2) = l2(v2)
   // an1(u1, vn) + an2(u2, vn) + ann(un, vn) = ln(vn)
-  wf.add_biform(0, 0, callback(J_euler_DFcDYc), UNSYM, ANY, 1, &phii);
+  if (TIME_DISCR == 1) {
+    wf.add_liform(0, callback(Fc_euler), ANY, 3, &Cp, &Ci, &phii);
+    wf.add_biform(0, 0, callback(J_euler_DFcDYc), UNSYM, ANY, 1, &phii);
+  } else {
+    wf.add_liform(0, callback(Fc_cranic), ANY, 4, &Cp, &Ci, &phii, &phip);
+    wf.add_biform(0, 0, callback(J_cranic_DFcDYc), UNSYM, ANY, 2, &phii, &phip);
+  }
+  wf.add_liform_surf(1, callback(linear_form_surf_top), TOP_MARKER);
   wf.add_biform(1, 1, callback(J_euler_DFphiDYphi), UNSYM);
   wf.add_biform(0, 1, callback(J_euler_DFcDYphi), UNSYM, ANY, 1, &Ci);
   wf.add_biform(1, 0, callback(J_euler_DFphiDYc), UNSYM);
-
-  wf.add_liform(0, callback(Fc_euler), ANY, 3, &Cp, &Ci, &phii);
   wf.add_liform(1, callback(Fphi_euler), ANY, 2, &Ci, &phii);
 
-  wf.add_liform_surf(1, callback(linear_form_surf_top), TOP_MARKER);
   
   // Nonlinear solver
   UmfpackSolver umfpack;
